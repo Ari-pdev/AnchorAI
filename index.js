5a74171b4a7dcc6c52145716fa26604e442e3dd5
ari <communeofstars@gmail.com>
2024-10-20 22:06:34 +0530

    Add API wrapper for gemini

--- index.js
+++ index.js
@@ -16,17 +16,50 @@ const unlockInput = () => {
   input.placeholder = regularText;
 };
 
+const dealWithUserInput = (content) => {
+  try {
+    pywebview.api.get_bot_response(content).then(
+      (response) => {
+        unlockInput();
+        return response;
+      },
+    );
+  } catch (error) {
+    pywebview.api.log(error);
+    return "Something went wrong, most likely a network error. Please try later!";
+  }
+};
+
 const addBotMessage = (content) => {
   const newMessage = document.createElement("div");
-  newMessage.className = "Message Bot";
+  newMessage.className = "Message Bot Waiting";
 
   const now = new Date();
   const hours = String(now.getHours()).padStart(2, "0");
   const minutes = String(now.getMinutes()).padStart(2, "0");
   const currentTime = `${hours}:${minutes}`;
+  const subTime = `<sub> ${currentTime}</sub> `;
 
-  newMessage.innerHTML = content + `<sub> ${currentTime}</sub> `;
+  newMessage.innerHTML = " Waiting for AI to respond" + subTime;
   ChatWindowElement.appendChild(newMessage);
+
+  try {
+    pywebview.api.get_bot_response(content).then(
+      (response) => {
+        unlockInput();
+
+        newMessage.className = "Message Bot";
+        newMessage.innerHTML = response + subTime;
+      },
+    );
+  } catch (error) {
+    pywebview.api.log(error);
+
+    newMessage.className = "Message Bot";
+    newMessage.innerHTML =
+      "Something went wrong, most likely a network error. Please try later!" +
+      subTime;
+  }
 };
 
 const addUserMessage = () => {
@@ -46,19 +79,8 @@ const addUserMessage = () => {
 
   newMessage.innerHTML = content + `<sub> ${currentTime}</sub> `;
   ChatWindowElement.appendChild(newMessage);
-  try {
-    pywebview.api.get_bot_response(content).then(
-      (response) => {
-        addBotMessage(response);
-        unlockInput();
-      },
-    );
-  } catch (error) {
-    addBotMessage(
-      "Something went wrong, most likely a network error. Please try later!",
-    );
-    pywebview.api.log(error);
-  }
+
+  addBotMessage(content);
 };
 
 sendButton.addEventListener("click", addUserMessage);
--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 import webview
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+genai.configure(api_key=API_KEY)
+
+generation_config = {
+    "temperature": 2,
+    "top_p": 0.95,
+    "top_k": 64,
+    "max_output_tokens": 8192,
+    "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+    model_name="gemini-1.5-flash",
+    generation_config=generation_config,
+)
+
+chat_session = model.start_chat(
+    history=[]
+)
 
 
 class JS_Python_Bridge_API():
@@ -6,17 +31,14 @@ class JS_Python_Bridge_API():
         print(value)
 
     def get_bot_response(self, user_input: str):
-        return user_input
+        print(chat_session.history)
+        return chat_session.send_message(user_input).text
 
 
 def start_webview():
-    window = webview.create_window('Test', url="index.html",
-                                   js_api=JS_Python_Bridge_API())
-    webview.start(debug=True)
+    webview.create_window('Test', url="index.html",
+                          js_api=JS_Python_Bridge_API())
+    webview.start()
 
 
-def main():
-    start_webview()
-
-
-main()
+start_webview()
